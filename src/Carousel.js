b376cf13baf1544695da8d967674014bac3adae2
Rajendra Malya <malya236@gmail.com>
2020-12-26 22:14:58 +0530

    code for filter and reading data from json file

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,12 +7,12 @@ import {
     CardMedia,
     Typography,
     Grid,
-    Checkbox,
-    FormControlLabel,
     InputLabel,
     Select,
-    MenuItem 
+    MenuItem
 } from '@material-ui/core';
+
+import data from './data.json';
 
 function Banner(props) {
     if (props.newProp) console.log(props.newProp)
@@ -27,10 +27,6 @@ function Banner(props) {
                 <Typography className="Title">
                     {props.item.Name}
                 </Typography>
-
-                <Typography className="Caption">
-                    {props.item.Caption}
-                </Typography>
             </CardContent>
         </Grid>
     )
@@ -43,7 +39,6 @@ function Banner(props) {
             <Grid item xs={12 / totalItems} key={item.Name}>
                 <CardMedia
                     className="Media"
-                    image={item.Image}
                     title={item.Name}
                 >
                     <CardContent className="Content">
@@ -66,8 +61,13 @@ function Banner(props) {
 
     if (contentPosition === "left") {
         items.unshift(content);
+
+        //this will apply when item->contentPosition->middle
     } else if (contentPosition === "right") {
         items.push(content);
+
+        
+        //this will apply when item->contentPosition->right
     } else if (contentPosition === "middle") {
         items.splice(items.length / 2, 0, content);
     }
@@ -90,84 +90,25 @@ class CarouselSlider extends React.Component {
         super(props);
 
         this.state = {
-            autoPlay: false,
-            timer: 0,
-            animation: "fade",
-            indicators: true,
-            timeout: 0,
-            navButtonsAlwaysVisible: false,
-            navButtonsAlwaysInvisible: false,
-            category:null,
-            newItems:[],
-            items:[
-                {
-                    Name: "Electronics",
-                    Caption: "Electrify your friends!",
-                    contentPosition: "left",
-                    Category: "EC",
-                    Items: [
-                        {
-                            Name: "Macbook Pro",
-                            Price: "10000",
-                            Image: "/assets/images/1.jpg"
-                        },
-                        {
-                            Name: "iPhone",
-                            Price: "10000",
-                            Image: "./assets/images/1.jpg"
-                        }
-                    ]
-                },
-                {
-                    Name: "Home Appliances",
-                    Caption: "Say no to manual home labour!",
-                    contentPosition: "middle",
-                    Category: "HOME",
-                    Items: [
-                        {
-                            Name: "Washing Machine WX9102",
-                            Price: "10000",
-                            Image: "./assets/images/1.jpg"
-                        },
-                        {
-                            Name: "Learus Vacuum Cleaner",
-                            Price: "10000",
-                            
-                            Image: "./assets/images/1.jpg"
-                        }
-                    ]
-                },
-                {
-                    Name: "Decoratives",
-                    Caption: "Give style and color to your living room!",
-                    contentPosition: "right",
-                    Category: "DEC",
-                    Items: [
-                        {
-                            Name: "Living Room Lamp",
-                            Price: "10000",
-                            Image: "https://source.unsplash.com/featured/?lamp"
-                        },
-                        {
-                            Name: "Floral Vase",
-                            Price: "10000",
-                            Image: "https://source.unsplash.com/featured/?vase"
-                        }
-                    ]
-                }
-            ]
+            category: null,
+            items:data
         }
-        
-
+        this.newItems = this.state.items;
     }
 
 
-  handleChange = (event) => {
-    this.items=this.state.newItems.filter(item=>item.Category==event.target.value);
-    this.setState({items:this.items,category:event.target.value});
-  };
+    handleChange = (event) => {
+        const allitems = this.state.items;
+        if (event.target && event.target.value === "") {
+            this.newItems = allitems;
+        } else {
+            this.newItems = allitems.filter(item => item.Category === event.target.value);
+        }
+        this.setState({ category: event.target.value });
+    };
 
     render() {
+        this.newItems1 = this.state.items;
         return (
             <div style={{ marginTop: "50px", color: "#494949" }}>
                 <h2>
@@ -176,8 +117,8 @@ class CarouselSlider extends React.Component {
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
                         value={this.state.category}
-                        onChange={this.handleChange}
-                    >
+                        onChange={this.handleChange}>
+                        <MenuItem value=""><em>ALL</em></MenuItem>
                         <MenuItem value={'EC'}>Electronics</MenuItem>
                         <MenuItem value={'HOME'}>Home Appliances</MenuItem>
                         <MenuItem value={'DEC'}>Decoratives</MenuItem>
@@ -185,19 +126,15 @@ class CarouselSlider extends React.Component {
                 </h2>
 
                 <Carousel
-                    className="Example"
-                    autoPlay={this.state.autoPlay}
-                    timer={this.state.timer}
-                    animation={this.state.animation}
-                    indicators={this.state.indicators}
-                    timeout={this.state.timeout}
-                    navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
-                    navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
-                >
+                    timer={0}
+                    timeout={0}
+                    navButtonsAlwaysVisible={false}
+                    navButtonsAlwaysInvisible={false}>
                     {
-                        this.state.items.map((item, index) => {
+                        this.newItems.map((item, index) => {
                             return <Banner item={item} key={index} contentPosition={item.contentPosition} />
                         })
+
                     }
                 </Carousel>
             </div>
